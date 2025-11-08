6cf09bfda7ac3ec6ac3e1964a8eed50d22b78434
sirlin7777777-afk <sirlin7777777@gmail.com>
2025-11-08 04:22:34 +0300

    Add files via upload

---
+++ templates/Return Spoofs.lua
@@ -0,0 +1,9 @@
+--// Sigma Spy custom remote responces
+--// The Return table will be unpacked for the responce
+
+return {
+	-- [game.ReplicatedStorage.Remotes.HelloWorld] = {
+	-- 	Method = "FireServer",
+	-- 	Return = {"Hello world from Sigma Spy!"}
+	-- }
+}
